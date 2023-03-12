301348445e9ad18891b45487c30cef81f637c44e
RutkisXd <rutkauskas.jon@gmail.com>
2023-03-12 09:48:35 +0200

    Review-page-start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from './Pages/HomePage';
+import RestaurantDetailPage from './Pages/RestaurantDetailPage';
+import RestaurantsPage from './Pages/ResutantsPage';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './Components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/restaurants" element={<RestaurantsPage />} />
+          <Route path="/restaurants/:restaurantId" element={<RestaurantDetailPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
